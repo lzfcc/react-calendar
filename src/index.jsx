2c53a66e0965c7bc88f01d1d71ffb213ac9ac506
zifengliu <zifengliu@microsoft.com>
2024-04-10 16:38:25 +0800

    fix build error: could not find Could not find a required file.   Name: index.js.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Frontend/App';
+import reportWebVitals from './Frontend/reportWebVitals';
 
 const container = document.getElementById('root')
 const root = createRoot(container)
